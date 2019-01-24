cd413b87e27cfa25d5cffce10218532f87283641
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-01-24 13:23:27 -0800

    main and navbar handlebars added with bootstrap

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
+// Requirements 
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
 
+// handlebars
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+
+// routes
+app.get('/', (req, res) => res.render('home'))
+
+// port
 app.listen(port, () => 
 console.log(`Example app listening on port ${port}!`))
--- steps.md
+++ steps.md
@@ -5,3 +5,5 @@
 * [ ] Implement more intricate tests for CRUD apps as well as for authentication flows.
 
 1. `npm init, npm install express --save`
+2. `npm install express-handlebars --save`
+    var exphbs = require('express-handlebars');
