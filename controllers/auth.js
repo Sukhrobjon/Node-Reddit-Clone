be3bb78fdbde8bc214edc7ad273f2bcfdf6ec87a
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-01-27 12:07:36 -0800

    auth controller and user model implemented

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,27 @@
+const User = require('../models/user.js')
+
 module.exports = (app) => {
     // SIGN UP FORM
     app.get("/sign-up", (req, res) => {
         res.render("sign-up");
     });
+
+    // SIGN UP POST
+    app.post("/sign-up", (req, res) => {
+        
+        // Create User
+        const user = new User(req.body);
+
+        // save the user to database
+        user
+            .save()
+            .then(user => {
+                res.redirect("/");
+            })
+            .catch(err => {
+                console.log(err.message);
+            });
+    });
+
+
 }
