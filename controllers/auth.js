219b45cdfa91f7453292a6dd0c3bf21b78b42523
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-01-27 13:28:38 -0800

    Page 7:user can login completed

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,17 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/user.js')
 
+/*
+TO-DO:
+
+There is a lot more to make a full fledged authentication 
+system.See which ones you want to try:
+
+1. Add a Remember Me checkbox.What is the difference from when it is checked or not ?
+2. Require a password confirmation field.
+
+3. Plan out how you would do a "forget password" process.
+*/
 module.exports = (app) => {
     // SIGN UP FORM
     app.get("/sign-up", (req, res) => {
@@ -25,10 +36,47 @@ module.exports = (app) => {
     });
 
 
-      // LOGOUT
+    // LOGOUT
     app.get('/logout', (req, res) => {
         res.clearCookie('nToken');
         res.redirect('/');
     });
+
+    // LOGIN FORM
+    app.get('/login', (req, res) => {
+        res.render('login');
+    });
+
+    // LOGIN
+    app.post("/login", (req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+        // Find this user name
+        User.findOne({ username }, "username password")
+            .then(user => {
+                if (!user) {
+                    // User not found
+                    return res.status(401).send({ message: "Wrong Username or Password" });
+                }
+                // Check the password
+                user.comparePassword(password, (err, isMatch) => {
+                    if (!isMatch) {
+                    // Password does not match
+                    return res.status(401).send({ message: "Wrong Username or password" });
+                    }
+                    // Create a token
+                    const token = jwt.sign({ _id: user._id, username: user.username }, process.env.SECRET, {
+                    expiresIn: "60 days"
+                    });
+                    // Set a cookie and redirect to root
+                    res.cookie("nToken", token, { maxAge: 900000, httpOnly: true });
+                    res.redirect("/");
+                });
+            })
+        .catch(err => {
+            console.log(err);
+        });
+    });
+
 
 }
