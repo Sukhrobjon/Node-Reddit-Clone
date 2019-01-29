bb7de25f9e768d1be3feedb382e34b3b27b12999
Sukhrobjon <sgolibbo@mail.ccsf.edu>
2019-01-28 22:16:51 -0800

    author is displayed on index page

--- controllers/comments.js
+++ controllers/comments.js
@@ -1,28 +1,57 @@
 const Post = require('../models/post.js')
 const Comment = require('../models/comment.js')
+const User = require('../models/user')
+
+// TO-DO: Check with Dani if comment is assosiated with author
+
 
 module.exports = function(app) {
     // CREATE Comment
-app.post("/posts/:postId/comments", function (req, res) {
-    // INSTANTIATE INSTANCE OF MODEL
-    const comment = new Comment(req.body);
+    app.post('/posts/:postId/comments', function (req, res) {
+        //creating instance of model
+        if (req.user) {
+            const comment = new Comment(req.body)
+            comment.author = req.user._id;
+            //save instance of comment model to DB
+            comment
+                .save()
+                .then((comment) => {
+                    console.log(req.body)
+                    return Post.findById(req.params.postId)
 
-    // SAVE INSTANCE OF Comment MODEL TO DB
-    comment
-        .save()
-        .then(comment => {
-            return Post.findById(req.params.postId);
-        })
-        .then(post => {
-            post.comments.unshift(comment);
-            return post.save();
-        })
-        .then(post => {
-            res.redirect(`/`);
-        })
-        .catch(err => {
-            console.log(err);
-        });
-});
+                }).then(post => {
+                    console.log('crashing here')
+                    console.log(post)
+
+                    post.comments.unshift(comment);
+                    console.log('here')
+                    return post.save();
+                    // return Post.findById(req.params.postId);
+                }).then(comment => {
+                    // post.comments.unshift(comment)
+                    // return post.save()
+                    return User.findById(req.user._id);
+                })
+                .then(user => {
+                    console.log(user)
+                    user.comments.unshift(comment);
+                    user.save();
+                    res.redirect('/');
+                    // res.redirect('/posts/' + req.params.postId); >> giving error of Comment validation failed: postId: Path `postId` is required.
+                })
+                .then(post => {
+                    res.redirect('/');
+                })
+                .catch(err => {
+                    console.log("oops")
+                    console.log(err.message);
+                })
+        } else {
+            return res.status(401).send({
+                message: "login first"
+            });
+        }
+    })
+
 
 };
--- controllers/posts.js
+++ controllers/posts.js
@@ -4,7 +4,8 @@ module.exports = (app) => {
     // INDEX
     app.get('/', function (req, res) {
         var currentUser = req.user;
-        Post.find()
+        console.log(req.cookies);
+        Post.find().populate('author')
             .then(posts => {
                 console.log(`currenUser: ${currentUser}`)
                 res.render('posts-index', { posts, currentUser });
@@ -16,25 +17,35 @@ module.exports = (app) => {
 
     // NEW
     app.get("/posts/new", function (req, res) {
-        var currentUser = req.user;
+        // var currentUser = req.user;
         res.render('posts-new', {})
     });
 
     // CREATE
-    app.post('/posts/new', function (req, res) {
+    app.post("/posts/new", (req, res) => {
         if (req.user) {
-            // INSTANTIATE INSTANCE OF POST MODEL
-            const post = new Post(req.body);
+            var post = new Post(req.body);
+            post.author = req.user._id;
 
-            // SAVE INSTANCE OF POST MODEL TO DB
-            post.save((err, post) => {
-                // REDIRECT TO THE ROOT
-                return res.redirect(`/`);
-            });
+            post
+                .save()
+                .then(post => {
+                    return User.findById(req.user._id);
+                })
+                .then(user => {
+                    user.posts.unshift(post);
+                    user.save();
+                    // REDIRECT TO THE NEW POST
+                    res.redirect(`/posts/${post._id}`);
+                })
+                .catch(err => {
+                    console.log(err.message);
+                });
         } else {
             return res.status(401); // UNAUTHORIZED
         }
     });
+
 
     // SHOW One post
     app.get("/posts/:id", function (req, res) {
@@ -51,13 +62,12 @@ module.exports = (app) => {
 
     // SUBREDDIT
     app.get("/n/:subreddit", function (req, res) {
+        
         Post.find({
                 subreddit: req.params.subreddit
             })
             .then(posts => {
-                res.render("posts-index", {
-                    posts
-                });
+                res.render("posts-index", { posts });
             })
             .catch(err => {
                 console.log(err);
